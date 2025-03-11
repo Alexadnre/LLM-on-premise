97adae866e59d0d6c06b657803a01d355a18ab18
Alexnadre <alexandrevidelaine.me@gmail.com>
2025-03-11 14:41:13 +0100

    jout du chargement de db_create + debug

--- src/db_create.py
+++ src/db_create.py
@@ -2,6 +2,7 @@ from langchain_community.vectorstores import Chroma
 from document_split import *
 import os
 import shutil
+from tqdm import tqdm
 
 CHROMA_PATH = os.getenv("CHROMA_PATH")
 DATA_PATH = os.getenv("DATA_PATH")
@@ -12,12 +13,13 @@ def save_to_chroma(chunks: list[Document]):
     if os.path.exists(CHROMA_PATH):
         shutil.rmtree(CHROMA_PATH)
 
-    
-    db = Chroma.from_documents(
-        documents=chunks,
-        embedding=EMBEDDING_MODEL,
-        persist_directory=CHROMA_PATH
-    )
+    embeddings = OllamaEmbeddings(model=EMBEDDING_MODEL)
+    db = Chroma(embedding_function=embeddings, persist_directory=CHROMA_PATH)
+
+    # Utilisation de tqdm pour afficher la progression
+    for chunk in tqdm(chunks, desc="Generating embeddings"):
+        db.add_documents([chunk])
+
     db.persist()
     print(f"Saved {len(chunks)} chunks to {CHROMA_PATH}.")
 
